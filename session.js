43be83b2a6efe6fbe000718cc9213d2aac5d2032
MiguelF27 <miguel.cunha240512@gmail.com>
2025-05-09 14:33:04 -0300

    .

--- session.js
+++ session.js
@@ -2,7 +2,6 @@
     const usuarioLogado = localStorage.getItem('usuario_logado');
 
     if (!usuarioLogado) {
-        alert('Sessão expirada ou não iniciada. Redirecionando para login.');
         window.location.href = './login.html';
     }
 })();
